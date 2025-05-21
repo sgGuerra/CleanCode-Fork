a6aedfe9f89a54139f1d15a240186e80e2f5624f
Luis Guerra <110359937+sgGuerra@users.noreply.github.com>
2025-05-21 08:34:45 -0500

    Delete config/secret_config.py

--- config/secret_config.py
+++
@@ -1,7 +0,0 @@
-# Do not expose your Neon credentials to the browser
-
-PGHOST='ep-jolly-paper-a41gmb9f-pooler.us-east-1.aws.neon.tech'
-PGDATABASE='neondb'
-PGUSER='neondb_owner'
-PGPASSWORD='npg_oRd0DM9eWKuw'
-PGPORT=5432
